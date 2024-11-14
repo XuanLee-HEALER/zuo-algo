f2f6e1ae9abfddd190d4577df4edb205c114657f
mouselee <mouse.tree.lee@gmail.com>
2024-11-14 15:41:26 +0800

    第六十一讲，从递归入手一维动态规划，解码方法I、II。未完成

--- examples/54-uni-dp-problem.rs
+++ examples/54-uni-dp-problem.rs
@@ -1,6 +1,15 @@
 use std::mem;
 
-fn main() {}
+fn main() {
+    // assert_eq!(
+    //     Solution::hard_num_decodings(String::from("7*9*3*6*3*0*5*4*9*7*3*7*1*8*3*2*0*0*6*")),
+    //     Solution::hard_num_decodings_3(String::from("7*9*3*6*3*0*5*4*9*7*3*7*1*8*3*2*0*0*6*"))
+    // );
+    assert_eq!(
+        Solution::hard_num_decodings(String::from("7*9*3*6*3*0*5*4*9*7*3*7*1*8*3*2*0*0*6*")),
+        Solution::hard_num_decodings_4(String::from("7*9*3*6*3*0*5*4*9*7*3*7*1*8*3*2*0*0*6*"))
+    );
+}
 
 struct Solution;
 
@@ -161,5 +170,245 @@ impl Solution {
             i -= 1;
         }
         dp[0]
+    }
+
+    pub fn num_decodings(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        // Self::num_decodings_1(&s, 0)
+        let mut dp = vec![-1; s.len()];
+        Self::num_decodings_2(&s, 0, &mut dp)
+    }
+
+    fn num_decodings_1(s: &[char], i: usize) -> i32 {
+        if i == s.len() {
+            1
+        } else if s[i] == '0' {
+            0
+        } else {
+            let mut res = Self::num_decodings_1(s, i + 1);
+            if i + 1 < s.len() && (s[i] as u8 - b'0') * 10 + (s[i + 1] as u8 - b'0') <= 26 {
+                res += Self::num_decodings_1(s, i + 2);
+            }
+            res
+        }
+    }
+
+    fn num_decodings_2(s: &[char], i: usize, dp: &mut [i32]) -> i32 {
+        if i == s.len() {
+            1
+        } else if s[i] == '0' {
+            0
+        } else if dp[i] != -1 {
+            dp[i]
+        } else {
+            dp[i] = Self::num_decodings_2(s, i + 1, dp);
+            if i + 1 < s.len() && (s[i] as u8 - b'0') * 10 + (s[i + 1] as u8 - b'0') <= 26 {
+                dp[i] += Self::num_decodings_2(s, i + 2, dp);
+            }
+            dp[i]
+        }
+    }
+
+    pub fn num_decodings_3(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        let n = s.len();
+        let mut dp = vec![0; n + 2];
+        dp[n] = 1;
+        for i in (0..s.len()).rev() {
+            if s[i] == '0' {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if i + 1 < s.len() && (s[i] as u8 - b'0') * 10 + (s[i + 1] as u8 - b'0') <= 26 {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        dp[0]
+    }
+
+    pub fn num_decodings_4(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        let n = s.len();
+        let mut next_next = 0;
+        let mut next = 1;
+        for i in (0..n).rev() {
+            let cur = if s[i] == '0' {
+                0
+            } else {
+                let mut cur = next;
+                if i + 1 < s.len() && (s[i] as u8 - b'0') * 10 + (s[i + 1] as u8 - b'0') <= 26 {
+                    cur += next_next;
+                }
+                cur
+            };
+            next_next = next;
+            next = cur;
+        }
+        next
+    }
+
+    pub fn hard_num_decodings(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        // Self::hard_num_decodings_1(&s, 0)
+        let mut dp = vec![-1; s.len()];
+        Self::hard_num_decodings_2(&s, 0, &mut dp)
+    }
+
+    fn hard_num_decodings_1(s: &[char], i: usize) -> i32 {
+        if i == s.len() {
+            1
+        } else if s[i] == '0' {
+            0
+        } else {
+            let mut res = 0;
+            if s[i] == '*' {
+                res += 9 * Self::hard_num_decodings_1(s, i + 1);
+                if i + 1 < s.len() {
+                    if s[i + 1] == '*' {
+                        res += 15 * Self::hard_num_decodings_1(s, i + 2);
+                    } else if (s[i + 1] as u8 - b'0') <= 6 {
+                        res += 2 * Self::hard_num_decodings_1(s, i + 2);
+                    } else {
+                        res += Self::hard_num_decodings_1(s, i + 2)
+                    }
+                }
+            } else {
+                res += Self::hard_num_decodings_1(s, i + 1);
+                if i + 1 < s.len() {
+                    if s[i + 1] == '*' {
+                        if s[i] == '1' {
+                            res += 9 * Self::hard_num_decodings_1(s, i + 2);
+                        } else if s[i] == '2' {
+                            res += 6 * Self::hard_num_decodings_1(s, i + 2);
+                        }
+                    } else if (s[i] as u8 - b'0') * 10 + s[i + 1] as u8 - b'0' <= 26 {
+                        res += Self::hard_num_decodings_1(s, i + 2);
+                    }
+                }
+            }
+            res
+        }
+    }
+
+    const MOD: i64 = 1_000_000_007;
+
+    fn hard_num_decodings_2(s: &[char], i: usize, dp: &mut [i64]) -> i32 {
+        if i == s.len() {
+            1
+        } else if s[i] == '0' {
+            0
+        } else if dp[i] != -1 {
+            dp[i] as i32
+        } else {
+            let mut res: i64 = 0;
+            if s[i] == '*' {
+                res += 9 * Self::hard_num_decodings_2(s, i + 1, dp) as i64;
+                if i + 1 < s.len() {
+                    if s[i + 1] == '*' {
+                        res += 15 * Self::hard_num_decodings_2(s, i + 2, dp) as i64;
+                    } else if (s[i + 1] as u8 - b'0') <= 6 {
+                        res += 2 * Self::hard_num_decodings_2(s, i + 2, dp) as i64;
+                    } else {
+                        res += Self::hard_num_decodings_2(s, i + 2, dp) as i64
+                    }
+                }
+            } else {
+                res += Self::hard_num_decodings_2(s, i + 1, dp) as i64;
+                if i + 1 < s.len() {
+                    if s[i + 1] == '*' {
+                        if s[i] == '1' {
+                            res += 9 * Self::hard_num_decodings_2(s, i + 2, dp) as i64;
+                        } else if s[i] == '2' {
+                            res += 6 * Self::hard_num_decodings_2(s, i + 2, dp) as i64;
+                        }
+                    } else if (s[i] as u8 - b'0') * 10 + s[i + 1] as u8 - b'0' <= 26 {
+                        res += Self::hard_num_decodings_2(s, i + 2, dp) as i64;
+                    }
+                }
+            }
+            dp[i] = res % Self::MOD;
+            dp[i] as i32
+        }
+    }
+
+    fn hard_num_decodings_3(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        let n = s.len();
+        let mut dp = vec![0_i64; n + 2];
+        dp[n] = 1;
+        for i in (0..n).rev() {
+            if s[i] == '0' {
+                dp[i] = 0;
+            } else if s[i] == '*' {
+                dp[i] += 9 * dp[i + 1];
+                if i + 1 < n {
+                    if s[i + 1] == '*' {
+                        dp[i] += 15 * dp[i + 2];
+                    } else if (s[i + 1] as u8 - b'0') <= 6 {
+                        dp[i] += 2 * dp[i + 2];
+                    } else {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            } else {
+                dp[i] += dp[i + 1];
+                if i + 1 < n {
+                    if s[i + 1] == '*' {
+                        if s[i] == '1' {
+                            dp[i] += 9 * dp[i + 2];
+                        } else if s[i] == '2' {
+                            dp[i] += 6 * dp[i + 2];
+                        }
+                    } else if (s[i] as u8 - b'0') * 10 + s[i + 1] as u8 - b'0' <= 26 {
+                        dp[i] += dp[i + 2];
+                    }
+                }
+            }
+            dp[i] %= Self::MOD;
+        }
+
+        dp[0] as i32
+    }
+
+    fn hard_num_decodings_4(s: String) -> i32 {
+        let s = s.chars().collect::<Vec<char>>();
+        let n = s.len();
+        let mut next_next = 0;
+        let mut next = 1;
+        for i in (0..n).rev() {
+            let mut cur = 0;
+            if s[i] != '0' {
+                if s[i] == '*' {
+                    cur += 9 * next;
+                    if i + 1 < n {
+                        if s[i + 1] == '*' {
+                            cur += 15 * next_next;
+                        } else if (s[i + 1] as u8 - b'0') <= 6 {
+                            cur += 2 * next_next;
+                        } else {
+                            cur += next_next;
+                        }
+                    }
+                } else {
+                    cur += next;
+                    if i + 1 < n {
+                        if s[i + 1] == '*' {
+                            if s[i] == '1' {
+                                cur += 9 * next_next;
+                            } else if s[i] == '2' {
+                                cur += 6 * next_next;
+                            }
+                        } else if (s[i] as u8 - b'0') * 10 + s[i + 1] as u8 - b'0' <= 26 {
+                            cur += next_next;
+                        }
+                    }
+                }
+            }
+            cur %= Self::MOD;
+            next_next = next;
+            next = cur;
+        }
+        next as i32
     }
 }
